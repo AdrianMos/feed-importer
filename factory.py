dcbb7ad6edf287f60fb876f1abf9f14383c215e6
AdrianMos <Adrian.Raul.Mos@gmail.com>
2016-06-04 13:42:44 +0300

    included Factory for object creation; some variable names reduced

---
+++ factory.py
@@ -0,0 +1,30 @@
+from articles import *
+from article import Article
+
+class Factory(object):
+    """Factory for articles objects"""
+    
+    def CreateArticlesObject(self, userInput):
+        
+        #TODO(ArianMos): include the use of str_to_class()
+        #TODO(AdrianMos): decouple from user interface
+        # return reduce(getattr, str.split("."), sys.modules[__name__]).
+        
+        if userInput=="2":
+            newObject = NANArticles("NAN")    
+        elif userInput=="3":
+            newObject = HDREArticles("HDRE")
+        elif userInput=="4":
+            newObject = BEBArticles("BEB")
+        elif userInput=="5":
+            newObject = BebeBrandsArticles("HBBA")
+        elif userInput=="6":
+            newObject = BabyShopsArticles("HMER")
+        elif userInput=="7":
+            newObject = KidsDecorArticles("HDEC")
+        elif userInput=="8":
+            newObject = HubnersArticles("HHUB")
+        else:
+            newObject = None
+        
+        return newObject;
--- main.py
+++ main.py
@@ -6,17 +6,13 @@ Created on 26.04.2014
 import os.path
 import sys
 import logging
-#from _stat import filemode
 #add the current folder to the python paths
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from article import Article
-#from haiducel.articles import Articles
-from articles import *
-#from haiducel.articles import HaiducelArticles
+from factory import Factory
 from export import Export
 from operations import Operations
-from pathbuilder import PathBuilder
 import time
 
 
@@ -37,7 +33,8 @@ def main():
     print("*** Actualizare date magazinul Haiducel ***")
     print("*******************************************")
     print("Adrian Mos, V 3.3, 01.06.2016\n")
-    
+
+    factory = Factory();    
     
     try:
         
@@ -54,32 +51,17 @@ def main():
                   )
             userInput = input('Introduceti numarul optiunii pentru a continua: >> ')
             
+
             if userInput=="1":
                 sys.exit("Program terminat.")
                 break
-            elif userInput=="2":
-                feed = NANArticles("NAN")    
-                break
-            elif userInput=="3":
-                feed = HDREArticles("HDRE")
-                break
-            elif userInput=="4":
-                feed = BEBArticles("BEB")
-                break
-            elif userInput=="5":
-                feed = BebeBrandsArticles("HBBA")
-                break
-            elif userInput=="6":
-                feed = BabyShopsArticles("HMER")
-                break
-            elif userInput=="7":
-                feed = KidsDecorArticles("HDEC")
-                break
-            elif userInput=="8":
-                feed = HubnersArticles("HHUB")
-                break
             else:
-                sys.exit("Optiune invalida. Program terminat.")
+                feed = factory.CreateArticlesObject(userInput)
+            
+                if feed is not None:
+                   break
+                else
+                   sys.exit("Optiune invalida. Program terminat.")
         
                 
         print("  Procesare articole de tipul " + feed.__class__.__name__ + '.')
@@ -128,26 +110,26 @@ def main():
         updatedArticles.paths = feed.paths    
         print("    Articole actualizate: "+ str(updateMessages.__len__())) 
         export1 = Export()
-        filenameArticlesToUpdate = feed.code + "/out/" + feed.code + ' articole existente cu modificari in pret sau status ' + time.strftime("%Y-%m-%d") + '.csv'
-        export1.ExportPriceAndAvailabilityAndMessages(updatedArticles, updateMessages, filenameArticlesToUpdate)
+        filename = feed.code + "/out/" + feed.code + ' articole existente cu modificari in pret sau status ' + time.strftime("%Y-%m-%d") + '.csv'
+        export1.ExportPriceAndAvailabilityAndMessages(updatedArticles, updateMessages, filename)
         
         print("\n*** Articole noi active")
         newArticles = Operations.SubstractArticles(feed, haiducelArticlesFiltered)
         newArticles = Operations.RemoveUnavailableArticles(newArticles)
         newArticles.paths = feed.paths
-        filenamenewArticles = feed.code + "/out/" + feed.code + ' articole noi ' + time.strftime("%Y-%m-%d") + '.csv'
+        filename = feed.code + "/out/" + feed.code + ' articole noi ' + time.strftime("%Y-%m-%d") + '.csv'
         print("    Articole noi in feed: " + str(newArticles.articleList.__len__()))
-        export1.ExportAllData(newArticles, filenamenewArticles)
+        export1.ExportAllData(newArticles, filename)
         
         print("\n*** Toate articolele din feed transformate in formatul nostru")
-        filenamefeedOurFormat = feed.code + "/out/" + feed.code + ' original ' + time.strftime("%Y-%m-%d") + '.csv'
-        export1.ExportAllData(feed, filenamefeedOurFormat)		
+        filename = feed.code + "/out/" + feed.code + ' original ' + time.strftime("%Y-%m-%d") + '.csv'
+        export1.ExportAllData(feed, filename)		
         
         print("\n*** Articole sterse din feed")
         removedArticles = Operations.SubstractArticles(haiducelArticlesFiltered, feed)
-        filenameArticlesToRemove = feed.code + "/out/" + feed.code + ' articole de sters ' + time.strftime("%Y-%m-%d") + '.csv'
+        filename = feed.code + "/out/" + feed.code + ' articole de sters ' + time.strftime("%Y-%m-%d") + '.csv'
         print("    Articole ce nu mai exista in feed: " + str(removedArticles.articleList.__len__()))
-        export1.ExportArticlesForDeletion (removedArticles, filenameArticlesToRemove)
+        export1.ExportArticlesForDeletion (removedArticles, filename)
         
         if eroriImport > 0:
             print("\n\n***    Au fost gasite "+ str(eroriImport) + " ERORI in feed. Exista articole neimportate. ANUNTATI distribuitorul. Detalii in log.")
@@ -164,41 +146,6 @@ def main():
     print("\n*** Program terminat ***")
     
     
-    
-    
-    
-    '''
-    articles = BabyDreamsArticles()
-    
-    articles.Import()
-    articles.CheckForDuplicity()
-    articles.ComputePrices()
-    articles.ComputeAvailability()
-    articles.ComputeCategory()
-                   --> read mappings from file
-                   
-    articles.ComputeDescription()
-    articles.ComputeImagePaths()
-    
-    articles.SaveNewArticles()
-    articles.SaveUpdatedArticles()
-    '''
-    
-    
-    
-    
-    '''
-    articlesDummy1 = Articles()
-    articlesDummy1.Add("5", "item2  ", 15, "Inactive", "category1", "NAN")
-    articlesDummy1.Add("2", "item1", 10, "Active", "category1", "NAN")
-    articlesDummy1.Add("3", "item2", 10, "Active", "category1", "NAN")
-    
-    articlesDummy2 = Articles()
-    articlesDummy2.Add("2", "item1", 10, "Inactive", "category1", "NAN")
-    articlesDummy2.Add("5", "item2", 10, "Active", "category1", "NAN")
-    articlesDummy2.Add("3", "item2", 20, "Active", "category1", "NAN")
-    '''
-
 
 
 if __name__ == '__main__':
