8add671e19a59bcc16fd43403bbd7e3631569e94
AdrianMos <Adrian.Raul.Mos@gmail.com>
2016-06-04 20:11:13 +0300

    export class - allignment cleaned

--- export.py
+++ export.py
@@ -22,9 +22,8 @@ class Export(object):
                    ("imageSmall" , "v_products_image")])
     
     for i in range(1, 13):
-        header.update([("image"+str(i) , "v_products_images_image_" + str(i))])
+        header.update([("image"+str(i), "v_products_images_image_" + str(i))])
                        
-    # default_data["image"+str(i)] = "v_products_images_image_" + str(i)
         
     header.update([("supplier" , "v_products_supplier"),
                    ("price" , "v_products_price"),
@@ -48,49 +47,57 @@ class Export(object):
     def ExportAllData(self, articles, filename):
         
         with open(filename, 'wt') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
-            #writer.writerow(list(self.header.values()))
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', 
+                                quoting=csv.QUOTE_NONNUMERIC,
+                                lineterminator='\n')
             writer.writerow(list(self.header.values()))
-           
             
            
             for a in articles.articleList:
                 
-                row = [a.id, a.title, a.description, a.title, a.description, a.imagesNew[0], 
-                       a.imagesNew[1], a.imagesNew[2], a.imagesNew[3], a.imagesNew[4], a.imagesNew[5], a.imagesNew[6],
-                       a.imagesNew[7], a.imagesNew[8], a.imagesNew[9], a.imagesNew[10], a.imagesNew[11], a.imagesNew[12],
-                       
+                row = [a.id, a.title, a.description, a.title, 
+                       a.description, a.imagesNew[0], 
+                       a.imagesNew[1], a.imagesNew[2], a.imagesNew[3], 
+                       a.imagesNew[4], a.imagesNew[5], a.imagesNew[6],
+                       a.imagesNew[7], a.imagesNew[8], a.imagesNew[9], 
+                       a.imagesNew[10], a.imagesNew[11], a.imagesNew[12],
                        a.supplier, a.price, a.quantity, a.weight,
                        a.category, a.category, a.subcategory, a.subcategory,
                        a.available, "EOREOR"]
                 writer.writerow(row)
                 
-        print ("    Fisierul cu articole a fost salvat:    " + filename)
+        print ("    Fisier salvat:\n      \t" + filename)
 
     def ExportDataForOnlineshop(self, articles, filename):
             
         with open(filename, 'wt') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', 
+                                quoting=csv.QUOTE_NONNUMERIC, 
+                                lineterminator='\n')
             writer.writerow(list(self.header.values()))
             
             for a in articles.articleList:
                 
-                row = [a.id, a.title, a.description, a.title, a.description, "", 
-                   a.images[0], a.images[1], a.images[2], a.images[3], a.images[4], a.images[5],
-                   a.images[6], a.images[7], a.images[8], a.images[9], a.images[10], a.images[11],
-                   a.supplier, a.price, a.quantity, a.weight,
-                   a.category, a.category, a.subcategory, a.subcategory,
-                   a.available, "EOREOR"]
+                row = [a.id, a.title, a.description, a.title, 
+                       a.description, "", 
+                       a.images[0], a.images[1], a.images[2], a.images[3], 
+                       a.images[4], a.images[5], a.images[6], a.images[7], 
+                       a.images[8], a.images[9], a.images[10], a.images[11],
+                       a.supplier, a.price, a.quantity, a.weight,
+                       a.category, a.category, a.subcategory, a.subcategory,
+                       a.available, "EOREOR"]
                 writer.writerow(row)
                 
-        print ("    Fisierul cu articole pentru magazinul online a fost salvat:    " + filename)
+        print ("    Fisier salvat:\n      \t" + filename)
     
     def ExportPriceAndAvailability(self, articles, filename):
         
         with open(filename, 'wt') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-            #writer.writerow(list(self.header.values()))
-            writer.writerow([self.header["id"], self.header["price"], self.header["available"], self.header["eor"]])
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', 
+                                quoting=csv.QUOTE_MINIMAL,
+                                lineterminator='\n')
+            writer.writerow([self.header["id"], self.header["price"], 
+                             self.header["available"], self.header["eor"]])
            
             for a in articles.articleList:
                 row = [a.id, a.price, a.available, "EOREOR"]
@@ -99,28 +106,37 @@ class Export(object):
     def ExportArticlesForDeletion(self, articles, filename):
         
         with open(filename, 'wt') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-            #writer.writerow(list(self.header.values()))
-            writer.writerow([self.header["id"], self.header["available"], self.header["eor"]])
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL,
+                                lineterminator='\n')
+            writer.writerow([self.header["id"], self.header["available"],
+                             self.header["eor"]])
            
             for a in articles.articleList:
                 row = [a.id, "Delete", "EOREOR"]
                 writer.writerow(row)              
                 
-        print ("    Fisierul cu articolele de sters a fost salvat:    " + filename)
+        print ("    Fisier salvat:\n      \t" + filename)
     
     
-    def ExportPriceAndAvailabilityAndMessages(self, articles, messages, filename):
+    def ExportPriceAndAvailabilityAndMessages(self, 
+                                              articles, 
+                                              messages,
+                                              filename):
         
         with open(filename, 'wt') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-            #writer.writerow(list(self.header.values()))
-            writer.writerow([self.header["id"], self.header["price"], self.header["available"], self.header["eor"], "", "Mesaj comparare"])
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL,
+                                lineterminator='\n')
+            writer.writerow([self.header["id"], self.header["price"], 
+                             self.header["available"], self.header["eor"], 
+                             "", "Mesaj comparare"])
            
             for a in articles.articleList:
-                row = [a.id, a.price, a.available, "EOREOR", "", messages[str(a.id)]]
+                row = [a.id, a.price, a.available, "EOREOR", 
+                      "", messages[str(a.id)]]
                 writer.writerow(row)
                 
-        print ("    Fisierul cu articole a fost salvat:    " + filename)
+        print ("    Fisier salvat:\n      \t" + filename)
     
         
